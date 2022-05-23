9780e5640cd8c18b9db544e8f942333d77e76cdb
Roger Holden <r.holden@chi.ac.uk>
2022-05-23 18:53:29 +0100

    button added - no action listener

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -6,12 +6,17 @@ public class MyFrame extends JFrame {
      MyFrame(){
 
         JLabel myLabel = new JLabel("This is a label");
+        JButton myButton = new JButton("Click me");
+        JPanel myPanel = new JPanel();
+        this.setContentPane(myPanel);
+        
 
         this.setTitle("Let's get GUI");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400,400);
-        this.add(myLabel);
-        this.setVisible(true);
+         this.add(myLabel);
+         this.add(myButton);
+         this.setVisible(true);
 
     }
 
