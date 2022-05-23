98ffa4a7b850dcedd0faaa55f921d15605f8af9b
Roger Holden <r.holden@chi.ac.uk>
2022-05-23 19:06:39 +0100

    basic action listener to change text on label

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,15 +1,21 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
+    JButton myButton;
+    JLabel myLabel;
+
+    MyFrame(){
+
+        myLabel = new JLabel("This is a label");
+        myButton = new JButton("Click me");
 
 
-     MyFrame(){
-
-        JLabel myLabel = new JLabel("This is a label");
-        JButton myButton = new JButton("Click me");
         JPanel myPanel = new JPanel();
         this.setContentPane(myPanel);
-        
+
+        myButton.addActionListener(this);
 
         this.setTitle("Let's get GUI");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,4 +27,12 @@ public class MyFrame extends JFrame {
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == myButton) {
+
+                this.myLabel.setText("Ouch, you clicked me!");
+
+            }
+    }
 }
